bfa877d2b751d0a184ae79da8df01d4a777dc518
guzalmusurmonova <burxonovsherzod0025@gmail.com>
2023-04-09 23:10:11 -0700

    02200

--- is_odd.py
+++ is_odd.py
@@ -1,3 +1,5 @@
 #Create a variable "number" and assign it a integer value.
-
-#Print 1 if the number is odd, otherwise print 0.
+number=87
+a=(number)%2
+#Print 1 if the number is even, otherwise print 0.
+print(a)
