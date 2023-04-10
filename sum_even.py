3aa5197d9cd9e7e9e0477f4db05f65fbc887bdae
guzalmusurmonova <burxonovsherzod0025@gmail.com>
2023-04-10 04:48:11 -0700

    0052

--- sum_even.py
+++ sum_even.py
@@ -1,6 +1,11 @@
 #A four-digit integer is given. Find the sum of even digits in it.
 #Create a variable "var_int" and assign it a four-digit integer value.
-
+var_int=9874
+x1=var_int%10
+x2=var_int%100//10
+x3=var_int%1000//100
+x4=var_int//1000
 #Create a variable "sum_even" and assign it 0.
-
+sum_even=(x1+1)%2*x1+(x2+1)%2*x2+(x3+1)%2*x3+(x4+1)%2*x4
+print(sum_even)
 #Find the sum of the even digits in the variable "var_int".
